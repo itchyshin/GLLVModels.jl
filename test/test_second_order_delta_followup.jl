itchyshin/GLLVM.jl

feat(delta): DRAFT Option A scaffold (species CI + SO cells)
Wire Delta _family_ci for disp_group=:species and point core070_second_order
twin cells at per-trait dispersion. Decision stays PENDING until paste
accept delta dispersion A; no public default flip, no D1 claim, no merge.

Co-authored-by: Cursor <cursoragent@cursor.com>

test/test_second_order_delta_followup.jl
@@ -1,5 +1,7 @@
-# Second-order follow-up batch: Delta-lognormal / Delta-Gamma shared-η Wald wiring.
+# Second-order follow-up: Delta shared-η Wald (#347) + Option A species packing scaffold.
 # R↔Julia SE receipts run only when GLLVM_PARITY_TESTS=1 (needs RCall + gllvmTMB).
+# Decision `2026-09-15-delta-dispersion-alignment-pending.md` stays PENDING until paste
+# `accept delta dispersion A` — this file does not claim acceptance or D1 pass.
 
 using GLLVM, Test, Random, Distributions
 
@@ -21,11 +23,29 @@ end
 @testset "second-order delta follow-up (shared predictor Wald)" begin
     @testset "Delta-lognormal: shared θ packing + public Wald" begin
         Y = _sim_delta_lognormal(5, 1, 100; seed = 171)
-        fit = fit_delta_lognormal_gllvm(Y; K = 1, predictor = :shared, iterations = 400)
+        fit = fit_delta_lognormal_gllvm(Y; K = 1, predictor = :shared,
+                                        disp_group = :shared, iterations = 400)
         @test fit.predictor === :shared
+        @test fit.disp_group === :shared
         ad = GLLVM._family_ci(fit, Y)
         @test length(ad.θ) == 5 + GLLVM.rr_theta_len(5, 1) + 1
         @test count(startswith("beta["), ad.names) == 5
+        ci = confint(fit, Y; method = :wald, parm = "beta")
+        @test length(ci.term) == 5
+        fin = isfinite.(ci.se)
+        @test count(fin) ≥ 3
+        @test all(ci.lower[fin] .< ci.estimate[fin] .< ci.upper[fin])
+    end
+
+    @testset "Delta-lognormal: species θ packing + public Wald (Option A scaffold)" begin
+        Y = _sim_delta_lognormal(5, 1, 100; seed = 173)
+        fit = fit_delta_lognormal_gllvm(Y; K = 1, predictor = :shared,
+                                        disp_group = :species, iterations = 400)
+        @test fit.disp_group === :species
+        @test fit.σ isa AbstractVector && length(fit.σ) == 5
+        ad = GLLVM._family_ci(fit, Y)
+        @test length(ad.θ) == 5 + GLLVM.rr_theta_len(5, 1) + 5
+        @test count(startswith("sigma["), ad.names) == 5
         ci = confint(fit, Y; method = :wald, parm = "beta")
         @test length(ci.term) == 5
         fin = isfinite.(ci.se)
@@ -49,12 +69,42 @@ end
                 Y[t, s] = rand(Gamma(α, μ / α))
             end
         end
-        fit = fit_delta_gamma_gllvm(Y; K = K, predictor = :shared, iterations = 400)
+        fit = fit_delta_gamma_gllvm(Y; K = K, predictor = :shared,
+                                    disp_group = :shared, iterations = 400)
         ad = GLLVM._family_ci(fit, Y)
         @test count(startswith("beta["), ad.names) == 5
         ci = confint(fit, Y; method = :wald, parm = "beta[1]")
         @test ci.term == ["beta[1]"]
         @test isfinite(ci.se[1])
+    end
+
+    @testset "Delta-Gamma: species θ packing + public Wald (Option A scaffold)" begin
+        Random.seed!(174)
+        p, K, n = 5, 1, 100
+        β = 0.3 .* randn(p)
+        Λ = 0.4 .* randn(p, K)
+        α = 2.5 .+ rand(p)
+        Z = randn(K, n)
+        η = β .+ Λ * Z
+        Y = zeros(p, n)
+        for t in 1:p, s in 1:n
+            π = 1 / (1 + exp(-η[t, s]))
+            if rand() < π
+                μ = exp(η[t, s])
+                Y[t, s] = rand(Gamma(α[t], μ / α[t]))
+            end
+        end
+        fit = fit_delta_gamma_gllvm(Y; K = K, predictor = :shared,
+                                    disp_group = :species, iterations = 400)
+        @test fit.disp_group === :species
+        @test fit.α isa AbstractVector && length(fit.α) == p
+        ad = GLLVM._family_ci(fit, Y)
+        @test length(ad.θ) == p + GLLVM.rr_theta_len(p, K) + p
+        @test count(startswith("alpha["), ad.names) == p
+        ci = confint(fit, Y; method = :wald, parm = "beta")
+        @test length(ci.term) == p
+        fin = isfinite.(ci.se)
+        @test count(fin) ≥ 3
     end
 
     @testset "R paired each-own-optimum cells (live Δ)" begin
@@ -69,11 +119,11 @@ end
             for cell_id in ("delta_lognormal", "delta_gamma")
                 d = run_one_cell(cell_id)
                 @test get(d, "skip_reason", nothing) === nothing
-                @test get(d, "parameterisation_gap", false) == true
+                # Option A scaffold: cells use Julia :species; gap flag cleared.
+                # Still ≠ D1 pass / ACCEPTED disposition until paste + remeasure.
+                @test get(d, "parameterisation_gap", true) == false
                 se_rel = d["se_max_relative_delta"]
                 @test se_rel !== nothing && isfinite(se_rel)
-                # Contract §4 D1 not asserted: shared Julia dispersion vs R per-trait
-                # (logLik Δ ≈ 1.9 / 2.6 on parity seeds) moves both optima.
             end
         end
     end

itchyshin/GLLVModels.jl

test/test_second_order_delta_followup.jl
@@ -1,5 +1,7 @@
-# Second-order follow-up batch: Delta-lognormal / Delta-Gamma shared-η Wald wiring.
+# Second-order follow-up: Delta shared-η Wald (#347) + Option A species packing scaffold.
 # R↔Julia SE receipts run only when GLLVM_PARITY_TESTS=1 (needs RCall + gllvmTMB).
+# Decision `2026-09-15-delta-dispersion-alignment-pending.md` stays PENDING until paste
+# `accept delta dispersion A` — this file does not claim acceptance or D1 pass.
 
 using GLLVModels, Test, Random, Distributions
 
@@ -21,11 +23,29 @@ end
 @testset "second-order delta follow-up (shared predictor Wald)" begin
     @testset "Delta-lognormal: shared θ packing + public Wald" begin
         Y = _sim_delta_lognormal(5, 1, 100; seed = 171)
-        fit = fit_delta_lognormal_gllvm(Y; K = 1, predictor = :shared, iterations = 400)
+        fit = fit_delta_lognormal_gllvm(Y; K = 1, predictor = :shared,
+                                        disp_group = :shared, iterations = 400)
         @test fit.predictor === :shared
+        @test fit.disp_group === :shared
         ad = GLLVModels._family_ci(fit, Y)
         @test length(ad.θ) == 5 + GLLVModels.rr_theta_len(5, 1) + 1
         @test count(startswith("beta["), ad.names) == 5
+        ci = confint(fit, Y; method = :wald, parm = "beta")
+        @test length(ci.term) == 5
+        fin = isfinite.(ci.se)
+        @test count(fin) ≥ 3
+        @test all(ci.lower[fin] .< ci.estimate[fin] .< ci.upper[fin])
+    end
+
+    @testset "Delta-lognormal: species θ packing + public Wald (Option A scaffold)" begin
+        Y = _sim_delta_lognormal(5, 1, 100; seed = 173)
+        fit = fit_delta_lognormal_gllvm(Y; K = 1, predictor = :shared,
+                                        disp_group = :species, iterations = 400)
+        @test fit.disp_group === :species
+        @test fit.σ isa AbstractVector && length(fit.σ) == 5
+        ad = GLLVModels._family_ci(fit, Y)
+        @test length(ad.θ) == 5 + GLLVModels.rr_theta_len(5, 1) + 5
+        @test count(startswith("sigma["), ad.names) == 5
         ci = confint(fit, Y; method = :wald, parm = "beta")
         @test length(ci.term) == 5
         fin = isfinite.(ci.se)
@@ -49,12 +69,42 @@ end
                 Y[t, s] = rand(Gamma(α, μ / α))
             end
         end
-        fit = fit_delta_gamma_gllvm(Y; K = K, predictor = :shared, iterations = 400)
+        fit = fit_delta_gamma_gllvm(Y; K = K, predictor = :shared,
+                                    disp_group = :shared, iterations = 400)
         ad = GLLVModels._family_ci(fit, Y)
         @test count(startswith("beta["), ad.names) == 5
         ci = confint(fit, Y; method = :wald, parm = "beta[1]")
         @test ci.term == ["beta[1]"]
         @test isfinite(ci.se[1])
+    end
+
+    @testset "Delta-Gamma: species θ packing + public Wald (Option A scaffold)" begin
+        Random.seed!(174)
+        p, K, n = 5, 1, 100
+        β = 0.3 .* randn(p)
+        Λ = 0.4 .* randn(p, K)
+        α = 2.5 .+ rand(p)
+        Z = randn(K, n)
+        η = β .+ Λ * Z
+        Y = zeros(p, n)
+        for t in 1:p, s in 1:n
+            π = 1 / (1 + exp(-η[t, s]))
+            if rand() < π
+                μ = exp(η[t, s])
+                Y[t, s] = rand(Gamma(α[t], μ / α[t]))
+            end
+        end
+        fit = fit_delta_gamma_gllvm(Y; K = K, predictor = :shared,
+                                    disp_group = :species, iterations = 400)
+        @test fit.disp_group === :species
+        @test fit.α isa AbstractVector && length(fit.α) == p
+        ad = GLLVModels._family_ci(fit, Y)
+        @test length(ad.θ) == p + GLLVModels.rr_theta_len(p, K) + p
+        @test count(startswith("alpha["), ad.names) == p
+        ci = confint(fit, Y; method = :wald, parm = "beta")
+        @test length(ci.term) == p
+        fin = isfinite.(ci.se)
+        @test count(fin) ≥ 3
     end
 
     @testset "R paired each-own-optimum cells (live Δ)" begin
@@ -69,11 +119,11 @@ end
             for cell_id in ("delta_lognormal", "delta_gamma")
                 d = run_one_cell(cell_id)
                 @test get(d, "skip_reason", nothing) === nothing
-                @test get(d, "parameterisation_gap", false) == true
+                # Option A scaffold: cells use Julia :species; gap flag cleared.
+                # Still ≠ D1 pass / ACCEPTED disposition until paste + remeasure.
+                @test get(d, "parameterisation_gap", true) == false
                 se_rel = d["se_max_relative_delta"]
                 @test se_rel !== nothing && isfinite(se_rel)
-                # Contract §4 D1 not asserted: shared Julia dispersion vs R per-trait
-                # (logLik Δ ≈ 1.9 / 2.6 on parity seeds) moves both optima.
             end
         end
     end