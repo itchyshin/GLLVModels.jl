itchyshin/GLLVM.jl

feat(d3): internal Stage 1 confirmatory pin plumbing (DRAFT #411)
Move pin substrate into src (not exported), add J1 multi-fixed refit
helpers behind paste gate, confirmatory test file with skipped refit
smoke, and runbook PENDING export checklist. No public loading_profile.

Co-authored-by: Cursor <cursoragent@cursor.com>

test/parity/loading_profile_confirmatory_substrate.jl
@@ -1,107 +1,10 @@
-# Stage-0 substrate helpers for confirmatory Λ profiling (D3). Test-only —
-# not part of the public GLLVM.jl API until Stage 1 + maintainer G0.
+# Stage-0 substrate helpers for confirmatory Λ profiling (D3).
+# Delegates to internal `src/loading_profile_confirmatory_internal.jl` so Stage 1
+# fitter wiring shares one implementation with tests.
 
-"""
-    lambda_constraint_is_pinned(M_user, n_traits, K) -> BitMatrix
+using GLLVM
 
-Match `gllvmTMB` `loading-profile.R`: user `NaN`/`missing` = free, numeric = pinned,
-plus engine strict-upper-triangle pins for `i <= min(n_traits, K)` and `k > i`.
-"""
-function lambda_constraint_is_pinned(
-    M_user::Union{Nothing,AbstractMatrix{<:Real}},
-    n_traits::Int,
-    K::Int,
-)
-    n_traits > 0 && K > 0 ||
-        throw(ArgumentError("n_traits and K must be positive"))
-    is_pinned = falses(n_traits, K)
-    if M_user !== nothing
-        size(M_user) == (n_traits, K) ||
-            throw(ArgumentError("pin matrix must be $(n_traits)×$(K)"))
-        for i in 1:n_traits, k in 1:K
-            v = M_user[i, k]
-            is_pinned[i, k] = !(v isa Real && isnan(v))
-        end
-    end
-    for i in 1:min(n_traits, K), k in 1:K
-        k > i && (is_pinned[i, k] = true)
-    end
-    return is_pinned
-end
-
-"""
-    normalize_lambda_constraint_pin_matrix(M) -> Matrix{Float64}
-
-Drop above-diagonal user entries in the first `min(p,K)` rows (R ignores them).
-Free slots stay `NaN`; fixed slots keep their numeric value.
-"""
-function normalize_lambda_constraint_pin_matrix(M::AbstractMatrix{<:Real})
-    p, K = size(M)
-    out = Matrix{Float64}(undef, p, K)
-    for i in 1:p, k in 1:K
-        if i <= min(p, K) && k > i
-            out[i, k] = NaN
-        else
-            v = M[i, k]
-            out[i, k] = v isa Real && isnan(v) ? NaN : Float64(v)
-        end
-    end
-    return out
-end
-
-"""
-    enumerate_free_lambda_entries(M_user, n_traits, K; entries=nothing)
-
-Return `Vector{Tuple{Int,Int}}` of `(trait, axis)` pairs R would profile.
-If `entries` is provided, validate each pair is free and return it (order preserved).
-"""
-function enumerate_free_lambda_entries(
-    M_user::Union{Nothing,AbstractMatrix{<:Real}},
-    n_traits::Int,
-    K::Int;
-    entries::Union{Nothing,AbstractMatrix{<:Integer}} = nothing,
-)
-    is_pinned = lambda_constraint_is_pinned(M_user, n_traits, K)
-    if entries === nothing
-        free = Tuple{Int,Int}[]
-        for i in 1:n_traits, k in 1:K
-            !is_pinned[i, k] && push!(free, (i, k))
-        end
-        return free
-    end
-    size(entries, 2) >= 2 ||
-        throw(ArgumentError("entries must have at least two columns (i, k)"))
-    out = Tuple{Int,Int}[]
-    for row in 1:size(entries, 1)
-        i = Int(entries[row, 1])
-        k = Int(entries[row, 2])
-        (1 <= i <= n_traits && 1 <= k <= K) ||
-            throw(ArgumentError("entry ($i, $k) out of range for $(n_traits)×$(K) Lambda"))
-        is_pinned[i, k] &&
-            throw(ArgumentError("entry ($i, $k) is pinned or structurally zero"))
-        push!(out, (i, k))
-    end
-    return out
-end
-
-"""
-    profile_refit_lambda_constraint(M_user, i, k, c) -> Matrix{Float64}
-
-Build the pin matrix for one profile grid point: preserve other user pins, set `(i,k)` to `c`.
-"""
-function profile_refit_lambda_constraint(
-    M_user::Union{Nothing,AbstractMatrix{<:Real}},
-    n_traits::Int,
-    K::Int,
-    i::Integer,
-    k::Integer,
-    c::Real,
-)
-    M = if M_user === nothing
-        fill(NaN, n_traits, K)
-    else
-        normalize_lambda_constraint_pin_matrix(M_user)
-    end
-    M[i, k] = Float64(c)
-    return M
-end
+const lambda_constraint_is_pinned = GLLVM._lambda_constraint_is_pinned
+const normalize_lambda_constraint_pin_matrix = GLLVM._normalize_lambda_constraint_pin_matrix
+const enumerate_free_lambda_entries = GLLVM._enumerate_free_lambda_entries
+const profile_refit_lambda_constraint = GLLVM._profile_refit_lambda_constraint

test/test_loading_profile_confirmatory.jl
@@ -1,0 +1,48 @@
+using Test
+using GLLVM
+using Random
+
+include("parity/fixtures/loading_profile_confirmatory_substrate.jl")
+
+const _P = LOADING_PROFILE_CONFIRMATORY_P
+const _K = LOADING_PROFILE_CONFIRMATORY_K
+
+@testset "loading_profile confirmatory internal (D3 Stage 1 plumbing)" begin
+    @testset "internal substrate matches Stage 0 fixtures" begin
+        pins = loading_profile_fixture_mask_b_pins()
+        free = GLLVM._enumerate_free_lambda_entries(pins, _P, _K)
+        @test free == LOADING_PROFILE_ORACLE_FREE_MASK_B_PINS
+
+        M = GLLVM._profile_refit_lambda_constraint(pins, _P, _K, 2, 1, 0.42)
+        @test M[2, 1] == 0.42
+        @test M[1, 1] == -0.8
+    end
+
+    @testset "paste gate for engine refit (default require_paste)" begin
+        @test !GLLVM._d3_loading_profile_stage1_paste_authorized()
+        rng = MersenneTwister(49)
+        p, n, K = 4, 8, 2
+        Y = randn(rng, p, n)
+        fit = fit_gaussian_gllvm(Y; K = K)
+        pins = loading_profile_fixture_mask_b_pins()
+        @test_throws ArgumentError GLLVM._confirmatory_profile_refit_lambda_pin(
+            fit, Y, pins, 2, 1, 0.1)
+    end
+
+    if GLLVM._d3_loading_profile_stage1_paste_authorized()
+        @testset "J1 pin-and-refit smoke (paste authorized)" begin
+            rng = MersenneTwister(49)
+            p, n, K = 4, 12, 2
+            Y = randn(rng, p, n)
+            fit = fit_gaussian_gllvm(Y; K = K)
+            pins = loading_profile_fixture_mask_b_pins()
+            ll, ok = GLLVM._confirmatory_profile_refit_lambda_pin(
+                fit, Y, pins, 2, 1, 0.05; require_paste = true)
+            @test ok
+            @test isfinite(ll)
+            @test ll <= fit.logLik + 1e-8
+        end
+    else
+        @test_skip "confirmatory pin refit smoke waits for paste G0 Stage 1"
+    end
+end

itchyshin/GLLVModels.jl

test/parity/loading_profile_confirmatory_substrate.jl
@@ -1,107 +1,11 @@
 # Stage-0 substrate helpers for confirmatory Λ profiling (D3). Test-only —
 # not part of the public GLLVModels.jl API until Stage 1 + maintainer G0.
+# Delegates to internal `src/loading_profile_confirmatory_internal.jl` so Stage 1
+# fitter wiring shares one implementation with tests.
 
-"""
-    lambda_constraint_is_pinned(M_user, n_traits, K) -> BitMatrix
+using GLLVModels
 
-Match `gllvmTMB` `loading-profile.R`: user `NaN`/`missing` = free, numeric = pinned,
-plus engine strict-upper-triangle pins for `i <= min(n_traits, K)` and `k > i`.
-"""
-function lambda_constraint_is_pinned(
-    M_user::Union{Nothing,AbstractMatrix{<:Real}},
-    n_traits::Int,
-    K::Int,
-)
-    n_traits > 0 && K > 0 ||
-        throw(ArgumentError("n_traits and K must be positive"))
-    is_pinned = falses(n_traits, K)
-    if M_user !== nothing
-        size(M_user) == (n_traits, K) ||
-            throw(ArgumentError("pin matrix must be $(n_traits)×$(K)"))
-        for i in 1:n_traits, k in 1:K
-            v = M_user[i, k]
-            is_pinned[i, k] = !(v isa Real && isnan(v))
-        end
-    end
-    for i in 1:min(n_traits, K), k in 1:K
-        k > i && (is_pinned[i, k] = true)
-    end
-    return is_pinned
-end
-
-"""
-    normalize_lambda_constraint_pin_matrix(M) -> Matrix{Float64}
-
-Drop above-diagonal user entries in the first `min(p,K)` rows (R ignores them).
-Free slots stay `NaN`; fixed slots keep their numeric value.
-"""
-function normalize_lambda_constraint_pin_matrix(M::AbstractMatrix{<:Real})
-    p, K = size(M)
-    out = Matrix{Float64}(undef, p, K)
-    for i in 1:p, k in 1:K
-        if i <= min(p, K) && k > i
-            out[i, k] = NaN
-        else
-            v = M[i, k]
-            out[i, k] = v isa Real && isnan(v) ? NaN : Float64(v)
-        end
-    end
-    return out
-end
-
-"""
-    enumerate_free_lambda_entries(M_user, n_traits, K; entries=nothing)
-
-Return `Vector{Tuple{Int,Int}}` of `(trait, axis)` pairs R would profile.
-If `entries` is provided, validate each pair is free and return it (order preserved).
-"""
-function enumerate_free_lambda_entries(
-    M_user::Union{Nothing,AbstractMatrix{<:Real}},
-    n_traits::Int,
-    K::Int;
-    entries::Union{Nothing,AbstractMatrix{<:Integer}} = nothing,
-)
-    is_pinned = lambda_constraint_is_pinned(M_user, n_traits, K)
-    if entries === nothing
-        free = Tuple{Int,Int}[]
-        for i in 1:n_traits, k in 1:K
-            !is_pinned[i, k] && push!(free, (i, k))
-        end
-        return free
-    end
-    size(entries, 2) >= 2 ||
-        throw(ArgumentError("entries must have at least two columns (i, k)"))
-    out = Tuple{Int,Int}[]
-    for row in 1:size(entries, 1)
-        i = Int(entries[row, 1])
-        k = Int(entries[row, 2])
-        (1 <= i <= n_traits && 1 <= k <= K) ||
-            throw(ArgumentError("entry ($i, $k) out of range for $(n_traits)×$(K) Lambda"))
-        is_pinned[i, k] &&
-            throw(ArgumentError("entry ($i, $k) is pinned or structurally zero"))
-        push!(out, (i, k))
-    end
-    return out
-end
-
-"""
-    profile_refit_lambda_constraint(M_user, i, k, c) -> Matrix{Float64}
-
-Build the pin matrix for one profile grid point: preserve other user pins, set `(i,k)` to `c`.
-"""
-function profile_refit_lambda_constraint(
-    M_user::Union{Nothing,AbstractMatrix{<:Real}},
-    n_traits::Int,
-    K::Int,
-    i::Integer,
-    k::Integer,
-    c::Real,
-)
-    M = if M_user === nothing
-        fill(NaN, n_traits, K)
-    else
-        normalize_lambda_constraint_pin_matrix(M_user)
-    end
-    M[i, k] = Float64(c)
-    return M
-end
+const lambda_constraint_is_pinned = GLLVModels._lambda_constraint_is_pinned
+const normalize_lambda_constraint_pin_matrix = GLLVModels._normalize_lambda_constraint_pin_matrix
+const enumerate_free_lambda_entries = GLLVModels._enumerate_free_lambda_entries
+const profile_refit_lambda_constraint = GLLVModels._profile_refit_lambda_constraint

test/test_loading_profile_confirmatory.jl
@@ -1,0 +1,48 @@
+using Test
+using GLLVModels
+using Random
+
+include("parity/fixtures/loading_profile_confirmatory_substrate.jl")
+
+const _P = LOADING_PROFILE_CONFIRMATORY_P
+const _K = LOADING_PROFILE_CONFIRMATORY_K
+
+@testset "loading_profile confirmatory internal (D3 Stage 1 plumbing)" begin
+    @testset "internal substrate matches Stage 0 fixtures" begin
+        pins = loading_profile_fixture_mask_b_pins()
+        free = GLLVModels._enumerate_free_lambda_entries(pins, _P, _K)
+        @test free == LOADING_PROFILE_ORACLE_FREE_MASK_B_PINS
+
+        M = GLLVModels._profile_refit_lambda_constraint(pins, _P, _K, 2, 1, 0.42)
+        @test M[2, 1] == 0.42
+        @test M[1, 1] == -0.8
+    end
+
+    @testset "paste gate for engine refit (default require_paste)" begin
+        @test !GLLVModels._d3_loading_profile_stage1_paste_authorized()
+        rng = MersenneTwister(49)
+        p, n, K = 4, 8, 2
+        Y = randn(rng, p, n)
+        fit = fit_gaussian_gllvm(Y; K = K)
+        pins = loading_profile_fixture_mask_b_pins()
+        @test_throws ArgumentError GLLVModels._confirmatory_profile_refit_lambda_pin(
+            fit, Y, pins, 2, 1, 0.1)
+    end
+
+    if GLLVModels._d3_loading_profile_stage1_paste_authorized()
+        @testset "J1 pin-and-refit smoke (paste authorized)" begin
+            rng = MersenneTwister(49)
+            p, n, K = 4, 12, 2
+            Y = randn(rng, p, n)
+            fit = fit_gaussian_gllvm(Y; K = K)
+            pins = loading_profile_fixture_mask_b_pins()
+            ll, ok = GLLVModels._confirmatory_profile_refit_lambda_pin(
+                fit, Y, pins, 2, 1, 0.05; require_paste = true)
+            @test ok
+            @test isfinite(ll)
+            @test ll <= fit.logLik + 1e-8
+        end
+    else
+        @test_skip "confirmatory pin refit smoke waits for paste G0 Stage 1"
+    end
+end